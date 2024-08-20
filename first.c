fb353253a4cd0feebbbf121a497e4b5271871965
Oleksandr-Berd <alex.berd86@gmail.com>
2024-08-20 09:18:39 +0300

    keep on

--- first.c
+++ first.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
+// int main(){
+//     int a = 3;
+//     float b = 4.5;
+//     double c = 5.25;
+//     float sum;
+
+//     sum = a + b + c;
+
+//     printf("Sum: %f\n", sum);
+
+//     return 0;
+// }
+
 int main(){
-    int a = 3;
-    float b = 4.5;
-    double c = 5.25;
-    float sum;
+    int average;
+    int grades[10];
+    int desirableNumber = 85;
 
-    sum = a + b + c;
+    grades[0] = 80;
+    grades[1] = 90;
+    grades[2] = desirableNumber * 3 - (grades[0] + grades[1]);
 
-    printf("Sum: %f\n", sum);
+    average = (grades[0] + grades[1] + grades[2]) / 3;
 
-    return 0;
+    printf("the average number is %d", average);
 }
