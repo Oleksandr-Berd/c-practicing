8ff51b0dc08f2ab7112bb283ea82877abe9c1b30
Oleksandr-Berd <alex.berd86@gmail.com>
2024-08-21 08:19:23 +0300

    keep on

--- first.c
+++ first.c
@@ -13,16 +13,53 @@
 //     return 0;
 // }
 
-int main(){
-    int average;
-    int grades[10];
-    int desirableNumber = 85;
+// int main(){
+//     int average;
+//     int grades[10];
+//     int desirableNumber = 85;
 
-    grades[0] = 80;
-    grades[1] = 90;
-    grades[2] = desirableNumber * 3 - (grades[0] + grades[1]);
+//     grades[0] = 80;
+//     grades[1] = 90;
+//     grades[2] = desirableNumber * 3 - (grades[0] + grades[1]);
 
-    average = (grades[0] + grades[1] + grades[2]) / 3;
+//     average = (grades[0] + grades[1] + grades[2]) / 3;
 
-    printf("the average number is %d", average);
-}
+//     printf("the average number is %d", average);
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     /* TODO: declare the 2D array grades here */
+//     float average;
+//     int i;
+//     int j;
+
+//     grades[0][0] = 80;
+//     grades[0][1] = 70;
+//     grades[0][2] = 65;
+//     grades[0][3] = 89;
+//     grades[0][4] = 90;
+
+//     grades[1][0] = 85;
+//     grades[1][1] = 80;
+//     grades[1][2] = 80;
+//     grades[1][3] = 82;
+//     grades[1][4] = 87;
+
+//     /* TODO: complete the for loop with appropriate terminating conditions */
+//     for (i = 0; i < ; i++)
+//     {
+//         average = 0;
+//         for (j = 0; j < ; j++)
+//         {
+//             average += grades[i][j];
+//         }
+
+//         /* TODO: compute the average marks for subject i */
+//         printf("The average marks obtained in subject %d is: %.2f\n", i, average);
+//     }
+
+//     return 0;
+// }
